f22dcd3ab435edfae3ed605cf619ffefbce3193b
Mark Balleby Hansen <mbal@verdo.com>
2020-02-25 13:34:19 +0100

    Created model

--- Terminal/DAL/DataLayer.cs
+++ Terminal/DAL/DataLayer.cs
@@ -1,13 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
 namespace DAL
 {
-    public class DataLayer
+    public class DataLayer : Iemployees, Iitems, Iorders, Ipermissions, Ireservations, Itables
     {
+        private string conn = "server=192.168.132.33;user id=db_user;database=restaurant;persistsecurityinfo=True";
+
+        public int id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string username { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string password { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string first_name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string last_name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int permId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int price { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int reservation_id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int item_id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int employee_id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int table_id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime date { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int seats { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime created { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void SQLString(string sqlstring)
+        {
+            SqlConnection con = new SqlConnection(conn);
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sqlstring, con);
+            cmd.ExecuteNonQuery();
+
+        }
+
+        public object ExecuteSqlString(string sqlstring)
+        {
+            SqlConnection con = new SqlConnection(conn);
+            con.Open();
+            DataTable dt = new DataTable();
+            SqlCommand cmd = new SqlCommand(sqlstring, con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            return dt;
+        }
+
+        //CRUD
+
+
+
+
+
+
 
     }
 }
--- Terminal/DAL/Model.cs
+++ Terminal/DAL/Model.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace DAL
 {
-    interface Interface1
+    interface Iemployees
     {
+        int id { get; set; }
+        string username { get; set; }
+        string password { get; set; }
+        string first_name { get; set; }
+        string last_name { get; set; }
+        int permId { get; set; }
+        DateTime created { get; set; }
+    }
+
+    interface Iitems
+    {
+        int id { get; set; }
+        string name { get; set; }
+        int price { get; set; }
+    }
+
+    interface Iorders
+    {
+        int id { get; set; }
+        int reservation_id { get; set; }
+        int item_id { get; set; }
+    }
+
+    interface Ireservations
+    {
+        int id { get; set; }
+        int employee_id { get; set; }
+        int table_id { get; set; }
+        DateTime date { get; set; }
+    }
+
+    interface Itables
+    {
+        int id { get; set; }
+        string name { get; set; }
+        int seats { get; set; }
+    }
+
+    interface Ipermissions
+    {
+        int id { get; set; }
+        string name { get; set; }
     }
 }

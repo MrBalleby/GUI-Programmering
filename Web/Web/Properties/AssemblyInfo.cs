e0e77dd7505ba78cd9f72f3345f84aa02d3029e2
Andreas <36854385+Andreas259@users.noreply.github.com>
2020-02-25 11:57:23 +0100

    Web

--- Web/Web/Properties/AssemblyInfo.cs
+++ Web/Web/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("cbd75a23-e0b1-4bcb-a8e3-23e25924b053")]
+[assembly: Guid("f58d0c9d-5f17-4304-87c7-96d9d47840b7")]
 
 // Version information for an assembly consists of the following four values:
 //
